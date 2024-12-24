1dead5a07fe85764da6452a51b0e777a1d1fdfe6
xulei <xl18845898715@gmail.com>
2024-12-24 17:23:11 +0800

    feat: add projects

--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function SectionHeading({ title }: { title: string }) {
-  return <h2 className="text-3xl font-medium capitalize mb-8">{title}</h2>;
+  return (
+    <h2 className="text-3xl font-medium capitalize mb-8 text-center">
+      {title}
+    </h2>
+  );
 }
