5256e72b5dfe9938b16596b694fbe20cae4a5832
xulei <xl18845898715@gmail.com>
2024-12-25 18:19:59 +0800

    fix: 注釋 context

--- context/active-section-context.tsx
+++ context/active-section-context.tsx
@@ -1,19 +1,19 @@
-import React, { createContext, useState } from 'react';
-import { links } from '@/lib/data';
+// import React, { createContext, useState } from 'react';
+// import { links } from '@/lib/data';
 
-type SectionName = (typeof links)[number]['name'];
-type ActiveSectionContextProviderProps = {
-  children: React.ReactNode;
-};
+// type SectionName = (typeof links)[number]['name'];
+// type ActiveSectionContextProviderProps = {
+//   children: React.ReactNode;
+// };
 
-const ActiveSectionContext = createContext(null);
+// const ActiveSectionContext = createContext(null);
 
-export default function ActiveSectionContextProvider({
-  children,
-}: ActiveSectionContextProviderProps) {
-  const [activeSection, setActiveAction] = useState<SectionName>('Home');
+// export default function ActiveSectionContextProvider({
+//   children,
+// }: ActiveSectionContextProviderProps) {
+//   const [activeSection, setActiveAction] = useState<SectionName>('Home');
 
-  return (
-    <ActiveSectionContext.Provider>{children}</ActiveSectionContext.Provider>
-  );
-}
+//   return (
+//     <ActiveSectionContext.Provider>{children}</ActiveSectionContext.Provider>
+//   );
+// }
